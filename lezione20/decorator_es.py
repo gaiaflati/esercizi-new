46f511c6afb886c8b9e68f47038de063d5efe9e0
gaiaflati <gaiaflati@live.it>
2024-07-05 19:34:31 +0200

    lez24

--- lezione20/decorator_es.py
+++ lezione20/decorator_es.py
@@ -133,4 +133,40 @@ prova_generatore = generatore()
 
 print(next(prova_generatore))
 
+                                
+
+                
+                
+
+                
+                
+
+import time
+    
+
+def funzione(id: int):
+    import time
+    import random
+    
+    sleep_time: int = random.randint(1, 10)
+    print(f"{id=} time {time.time()}")
+    time.sleep(sleep_time)
+    print(f"{id=} time {time.time()}")
+    
+if __name__ == "__main__":
+    
+    import threading
+    from concurrent.futures import ThreadPoolExecutor
+    
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        executor.map(funzione, range(100))
+   
+                
+
+                
+                
+
+                
+                
+
 
